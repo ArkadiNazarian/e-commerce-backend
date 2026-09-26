2fafbb0ffb360c765c684804903b415f172f78aa
ArkadiNazarian <nazarianarkadi@gmail.com>
2026-09-26 09:36:02 +0330

    add the upate user role and info routes

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -21,3 +21,81 @@ export const getUser = async (req: Request, res: Response) => {
         });
     }
 };
+
+export const updateUserRole = async (req: Request, res: Response) => {
+    try {
+
+        const { role } = req.body;
+
+        if (!req.user) {
+            return res.status(401).json({
+                success: false,
+                message: 'No user found with this email',
+            });
+        }
+
+        const findAndUpdateUserRole = await User.findByIdAndUpdate(req.user.id, { role }, { new: true, runValidators: true });
+
+        if (!findAndUpdateUserRole) {
+            return res.status(404).json({
+                success: false,
+                message: 'No user found with this email',
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: findAndUpdateUserRole,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+}
+
+export const updateUserInfo = async (req: Request, res: Response) => {
+    try {
+
+        if (!req.user) {
+            return res.status(401).json({
+                success: false,
+                message: 'No user found with this email',
+            });
+        }
+
+        if(req.body.role){
+            delete req.body.role
+        }
+
+        if(req.body.password){
+            delete req.body.password
+        }
+
+        const findAndUpdateUserInfo = await User.findByIdAndUpdate(
+            req.user.id,
+            { ...req.body },
+            { new: true, runValidators: true }
+        );
+
+        if (!findAndUpdateUserInfo) {
+            return res.status(404).json({
+                success: false,
+                message: 'No user found with this email',
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: findAndUpdateUserInfo,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+}
--- src/route/user.route.ts
+++ src/route/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { getUser } from '../controller/user.controller.js'
+import { getUser, updateUserInfo, updateUserRole } from '../controller/user.controller.js'
 import { protectedRoute } from '../controller/auth.controller.js'
 
 const userRoute = Router()
 
 userRoute.route('/me').get(protectedRoute, getUser)
+userRoute.route('/me/update-role').patch(protectedRoute, updateUserRole)
+userRoute.route('/me/update-user-info').patch(protectedRoute, updateUserInfo)
 
 export default userRoute
